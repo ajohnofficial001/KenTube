cf90f68221ee01678eeb8fbf23b8a9da0c16d6a2
John Adeyemo <ajohnofficial2020@gmail.com>
2022-11-06 12:21:40 +0100

    Initial layouts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./components";
 
 const Feed = () => {
-  return <div></div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        {" "}
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#ffffff" }}
+        >
+          {" "}
+          Copyright 2022 @johncankode
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New
+          <span style={{ color: "#F31503" }}> videos</span>
+        </Typography>
+
+        <Videos />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
