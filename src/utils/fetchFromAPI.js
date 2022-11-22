0878bd6ed717d53ead8b2aa773aafc09331a0d3b
John Adeyemo <ajohnofficial2020@gmail.com>
2022-11-22 17:54:07 +0100

    env file update and API consumption with Axios

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com/";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "623a3ffc47msh15c744f310e655ap1973dbjsn7a585f344adc",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchfromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
