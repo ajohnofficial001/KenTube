4ea6ef465c85671e9d38b5d5e29ec724206d3846
John Adeyemo <ajohnofficial2020@gmail.com>
2022-12-24 02:16:01 +0100

    Margin fix

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -16,7 +16,7 @@ const SearchFeed = () => {
   return (
     <Box
       p={2}
-      sx={{ overflowY: "auto", height: "90vh", flex: 2, marginTop: "6.5rem" }}
+      sx={{ overflowY: "auto", height: "90vh", flex: 2, marginTop: "4.6rem" }}
     >
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
         Search results for:
